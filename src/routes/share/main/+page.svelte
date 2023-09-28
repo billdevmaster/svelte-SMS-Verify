3bb2a9d7a5a1702d37817e5a6b4b6ab51927bfff
billdevmaster71 <billdevmaster@gmail.com>
2023-09-28 10:02:06 +0900

    for testing

--- src/routes/share/main/+page.svelte
+++ src/routes/share/main/+page.svelte
@@ -1,19 +1,19 @@
 
 <script>
 // @ts-nocheck
-
   import { onMount } from 'svelte';
   import InputForm from "../../../components/InputForm.svelte";
   import TextArea from "../../../components/TextArea.svelte";
   import Button from "../../../components/Button.svelte";
+  import LocationSearch from '../../../components/LocationSearch.svelte';
   import { goto } from "$app/navigation";
-
   import { collection, doc, setDoc, getFirestore, getDocs, query, where } from "firebase/firestore";
   import { user } from '../../../store';
 
   let firstname = "";
   let lastname = "";
   let city = "";
+  let geometry = "";
   let seeks = "";
 
   onMount(() => {
@@ -32,9 +32,17 @@
         lastname = item.data().lastname ? item.data().lastname : "";
         seeks = item.data().seeks ? item.data().seeks : "";
         city = item.data().city ? item.data().city : "";
+        geometry = item.data().geometry ? item.data().geometry : "";
       });
     }
-    getUser();
+    const checkApp = setInterval(() => {
+      user.subscribe(value => {
+        if (value) {
+          clearInterval(checkApp);
+          getUser();
+        }
+      });
+    }, 100);
   });
 
   const updateUser = async () => {
@@ -54,9 +62,10 @@
           firstname,
           lastname,
           city,
+          geometry,
           seeks,
-          provides: item.data().provides,
-          nickname: item.data().nickname,
+          provides: item.data().provides ? item.data().provides : "",
+          nickname: item.data().nickname ? item.data().nickname : "",
         });
         goto("/share/success");
       });
@@ -66,19 +75,24 @@
         firstname,
         lastname,
         city,
+        geometry,
         seeks
       });
       goto("/share/success");
     }
   }
 
+  const setLocation = (e) => {
+    geometry = `${e.detail.place.geometry.location.lat()}, ${e.detail.place.geometry.location.lng()}`; 
+    city = e.detail.selectedPrediction;
+  }
 </script>
 
 <div class="grid grid-cols-1">
   <p class="text-4xl font-bold text-center text-main mb-24">je recherche des talents</p>
   <InputForm label="Prénom" value={firstname} on:change={(e) => firstname=e.target.value} />
   <InputForm label="Nom" value={lastname} on:change={(e) => lastname=e.target.value} />
-  <InputForm label="Ville" value={city} on:change={(e) => city=e.target.value} />
+  <LocationSearch value={city}  on:placeChanged={setLocation}/>
   <TextArea label="Ma recherche:" value={seeks} on:change={(e) => seeks=e.target.value}  />
   <div class="mt-10">
     <Button on:click={updateUser}>Suivant</Button>
